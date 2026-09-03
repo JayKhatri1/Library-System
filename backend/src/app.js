37c71ce94b12438f31e85e847798b521e17e0721
JayKhatri1 <makvanejay1956@gmail.com>
2026-09-03 16:03:48 +0530

    Fix CORS for Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:
+        [
+            "http://localhost:5173",
+            "https://library-system-three-sigma.vercel.app"
+        ],
     credentials: true,
     allowedHeaders: [
         "Content-Type",
